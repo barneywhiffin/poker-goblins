1c1720758a413e32c0a35b5dbd11ea6624def3fe
Barney Whiffin <barneywhiffin@gmail.com>
2026-07-19 18:38:32 +0100

    feat: add deck

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,11 +1,22 @@
 import { useState } from "react"
 import "./table.css";
-import Card from "../card/Card";
+import TableCard from "../card/Card";
+import type { Card, Suit, Value } from "../../types/card"
 import CardSlot from "../card-slot/CardSlot";
 
 export default function Table() {
 
     const [cardShown, setCardShown] = useState(true);
+
+    const suits: Suit[] = ["Spades", "Diamonds", "Clubs", "Hearts"];
+    const values: Value[] = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    const deck: Card[] = [];
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < values.length; j++)
+        deck.push({suit: suits[i], value: values[j]})
+    }
+    console.log(deck);
 
     function toggleShown() {
         setCardShown(prevState => !prevState);
@@ -14,8 +25,8 @@ export default function Table() {
     return (
         <>
             <div className="table">
-                {/* <Card value="K" suit="Clubs" isShown={cardShown} toggleFunction={toggleShown}/>
-                <Card value="Q" suit="Hearts" isShown={cardShown} toggleFunction={toggleShown}/> */}
+                <TableCard value="K" suit="Clubs" isShown={cardShown} toggleFunction={toggleShown}/>
+                <TableCard value="Q" suit="Hearts" isShown={cardShown} toggleFunction={toggleShown}/>
                 <div className="board-centre">
                     <CardSlot horizontal={false}/>
                     <CardSlot horizontal={false}/>
