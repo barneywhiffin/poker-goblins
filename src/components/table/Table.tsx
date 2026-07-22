c941ca6042763f7cc08b5f20c71b1df7cf3d524f
Barney Whiffin <barneywhiffin@gmail.com>
2026-07-22 18:17:10 +0100

    feat: add 6 player slots

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -7,8 +7,24 @@ export default function Table({deck}: {deck: React.JSX.Element[]}) {
     const turnReached = true;
     const riverReached = true;
 
-    const [flopCard1, flopCard2, flopCard3, turnCard, riverCard] = deck;
+    const numPlayers = 4;
 
+    const playerSlots = [];
+
+    // for (let i = 0; i < numPlayers; i++) {
+    //     playerSlots.push(
+    //         <div className="player" style={{top: 100*i}}>
+    //             <CardSlot horizontal={false}/>
+    //             <CardSlot horizontal={false}/>
+    //         </div>
+    //     )
+    // }
+
+    // i think we need to manually fix all the possible slot positions
+    // then simply have a function to map players to the ones we are using
+
+    const [flopCard1, flopCard2, flopCard3, turnCard, riverCard] = deck;
+ 
     return (
         <>
             <div className="table">
@@ -20,6 +36,37 @@ export default function Table({deck}: {deck: React.JSX.Element[]}) {
                     {turnReached ? turnCard : <CardSlot horizontal={false}/>}                    
                     <div className="space"></div>
                     {riverReached ? riverCard : <CardSlot horizontal={false}/>}                    
+                </div>
+                {/* {playerSlots} */}
+
+                <div className="player" style={{top: "230px", left:"450px"}}>
+                    <CardSlot horizontal={false}/>
+                    <CardSlot horizontal={false}/>
+                </div>
+
+                <div className="player" style={{top: "230px", left:"200px"}}>
+                    <CardSlot horizontal={false}/>
+                    <CardSlot horizontal={false}/>
+                </div>
+
+                <div className="player" style={{top: "110px", left:"20px", flexDirection: "column"}}>
+                    <CardSlot horizontal={true}/>
+                    <CardSlot horizontal={true}/>
+                </div>
+
+                <div className="player" style={{top: "10px", left:"200px"}}>
+                    <CardSlot horizontal={false}/>
+                    <CardSlot horizontal={false}/>
+                </div>
+
+                <div className="player" style={{top: "10px", left:"450px"}}>
+                    <CardSlot horizontal={false}/>
+                    <CardSlot horizontal={false}/>
+                </div>
+
+                <div className="player" style={{top: "110px", left:"670px", flexDirection: "column"}}>
+                    <CardSlot horizontal={true}/>
+                    <CardSlot horizontal={true}/>
                 </div>
     
                 {/* TODO: add case switch with all the different table positions, and associated styling for location */}
