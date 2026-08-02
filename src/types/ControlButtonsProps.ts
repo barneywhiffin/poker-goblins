b4d9680ef561f8176874ecb61971e3899af951a2
Barney Whiffin <barneywhiffin@gmail.com>
2026-08-02 07:06:30 +0100

    style: improve game control buttons

---
+++ src/types/ControlButtonsProps.ts
@@ -0,0 +1,8 @@
+export interface ControlButtonsProps {
+    newRound: () => void,
+    toTheFlop: () => void,
+    toTheTurn: () => void,
+    toTheRiver: () => void,
+    showdown: () => void,
+    reset: () => void,
+}
