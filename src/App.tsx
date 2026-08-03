e108834cee1df12e05b6798359db03c41eafab33
Barney Whiffin <barneywhiffin@gmail.com>
2026-08-03 21:36:13 +0100

    style: add poker chip icons to page title

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { useState } from 'react';
 import LetsPlayPoker from './containers/LetsPlayPoker';
 import NavDrawer from './containers/NavDrawer';
+import VpipCalculator from './containers/vpip-calculator/VpipCalculator';
 import { createTheme, MantineProvider, ColorSchemeScript, AppShell, Burger, useComputedColorScheme } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { PokerChipIcon } from '@phosphor-icons/react';
 
 const theme = createTheme({
     fontFamily: '"Courier Prime", "Cutive Mono", "American Typewriter", "Courier New", monospace',
-	primaryColor: 'yellow',
+	primaryColor: 'orange',
 	focusRing: 'never',
 	autoContrast: true,
   	luminanceThreshold: 0.3,
@@ -21,7 +23,7 @@ function AppContent() {
 	
 	const [opened, { close, toggle }] = useDisclosure();
 	
-	const [activeTab, setActiveTab] = useState('poker');
+	const [activeTab, setActiveTab] = useState('vpip');
 	
 	// read only hook for check of current colour scheme: useComputedColorScheme
 	const computedColorScheme = useComputedColorScheme('light');
@@ -33,7 +35,7 @@ function AppContent() {
 			case 'sandbox':
 				return <p>sandbox</p>;
 			case 'vpip':
-				return <p>vpip</p>;
+				return <VpipCalculator/>;
 			case 'settings':
 				return <p>settings</p>;
 		}
@@ -47,12 +49,13 @@ function AppContent() {
 	return (
 		<AppShell header={{ height: headerHeight }}>
 			<AppShell.Header className='header'>
-				<div style={{display: 'flex', alignItems: 'center', maxWidth: '500px', height: headerHeight, marginLeft: 10}}>
-					<Burger opened={opened} onClick={toggle} color={computedColorScheme === 'light' ? 'dark' : 'yellow'}/>
-					<h1 style={{marginTop: '25px', marginLeft: 15, color: computedColorScheme === 'light' ? 'black' : 'orange'}}>Poker Goblins</h1>
+				<div style={{display: 'flex', alignItems: 'center', maxWidth: '500px', height: headerHeight, marginLeft: 14}}>
+					<Burger onClick={toggle} color={computedColorScheme === 'light' ? 'dark' : 'yellow'}/>
+					<h1 style={{marginTop: '26px', marginLeft: 17, color: computedColorScheme === 'light' ? 'black' : 'orange'}}>
+						P<PokerChipIcon size={22} style={{marginBottom: '3px'}}/>ker G<PokerChipIcon size={22} style={{marginBottom: '3px'}}/>blins</h1>
 				</div>
 			</AppShell.Header>
-				<NavDrawer openState={opened} activeTab={activeTab} navFunction={handleNavSelection} closeFunction={close}/>
+			<NavDrawer openState={opened} activeTab={activeTab} navFunction={handleNavSelection} closeFunction={close}/>
 			<AppShell.Main>
 				{mainContent()}
 			</AppShell.Main>
