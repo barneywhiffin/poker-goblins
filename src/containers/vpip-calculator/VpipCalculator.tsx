b5bb227e469e146b547b18af690aaaef2c318602
Barney Whiffin <barneywhiffin@gmail.com>
2026-08-04 07:39:34 +0100

    style: improve grid styling

--- src/containers/vpip-calculator/VpipCalculator.tsx
+++ src/containers/vpip-calculator/VpipCalculator.tsx
@@ -10,13 +10,13 @@ function sortValues(val1: string, val2: string) {
     const rank1 = mapValueToRank(val1);
     const rank2 = mapValueToRank(val2);
     if (rank2 > rank1) {
-        return val2 + val1 + 'o';
+        return val2 + '.' + val1 + 'o';
     }
     else if (rank2 < rank1) {
-        return val1 + val2 + 's';
+        return val1 + '.' + val2 + 's';
     }
     else {
-        return val1 + val2;
+        return val1 + '.' + val2;
     }
 }
 
@@ -29,9 +29,12 @@ export default function VpipCalculator() {
         for (let j = 0; j < values2.length; j++) {
             id++;
             spans.push(<VpipCard key={id} content={sortValues(values1[i], values2[j])}/>);
-            // spans.push(<VpipCard key={id} content={`${i} ${j}`}/>);
         }
     }
+
+    // the underlying div of each card could be a mantine button
+    // which takes a colour based argument, given via props
+    // this is held in state and cycled through on click
     
     return (
         <div className='vpip-grid'>
